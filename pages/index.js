cf4fb900772d85c88e09286a059dd1a59f61d90d
nuttawut <nuttawut@orisma.com>
2024-03-12 00:02:09 +0700

    edit  index

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from '@next/font/google'
-// import { Login } from '../components/Login'
-import { useAuth } from '../context/AuthContext'
-import { UserDashboard } from '../components/UserDashboard'
 import CardsPage from '../components/CardsPage'
-// import AddressForm from '../components/Address'
-// import styles from '../styles/Home.module.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  // const { currentUser } = useAuth();
-  // console.log('12',currentUser);
+
 
   return (
     <>
@@ -22,8 +16,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {/* {!currentUser && <Login />} */}
-      {/* {currentUser && <CardsPage/>} */}
       <CardsPage/>
     </>
   )
