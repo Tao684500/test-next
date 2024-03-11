1c3cab25d1210f403aa06e9e443134c7eecc1796
nuttawut <nuttawut@orisma.com>
2024-03-12 00:01:24 +0700

    edit  header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,3 @@
-// Header.js
-
 import React, { useState } from "react";
 import styles from "../styles/header.module.css";
 import { useAuth } from '../context/AuthContext';
@@ -23,21 +21,25 @@ export const Header = () => {
 
   return (
     <div className={styles.header}>
-      <h1 className="">TODO LIST</h1>
-      <div className={styles.dropdown}>
-        <i className={`fa-solid fa-user ${styles.icon} ${dropdownOpen ? styles.active : ''}`} onClick={toggleDropdown}></i>
-        <div className={`${styles.dropdownContent} ${dropdownOpen ? styles.show : ''}`}>
-          {currentUser ? (
-            <div>
-              <div>สวัสดีครับ</div>
-              <button onClick={handleLogoutClick}>Logout</button>
-            </div>
-          ) : (
-            <div>
-              <div>กรุณาล็อกอิน</div>
-              <button onClick={() => router.push('/login')}>Login</button>
-            </div>
-          )}
+      <h1 className="">NUTTAWUT TAO</h1>
+      <div className={styles.link}>
+        <div><a href="/">Home</a></div>
+        <div><a href="/UserDashboard">ToDo</a></div>
+        <div className={styles.dropdown}>
+          <i className={`fa-solid fa-user ${styles.icon} ${dropdownOpen ? styles.active : ''}`} onClick={toggleDropdown}></i>
+          <div className={`${styles.dropdownContent} ${dropdownOpen ? styles.show : ''}`}>
+            {currentUser ? (
+              <div>
+                <div>สวัสดีครับ</div>
+                <button onClick={handleLogoutClick}>Logout</button>
+              </div>
+            ) : (
+              <div>
+                <div>กรุณาล็อกอิน</div>
+                <button onClick={() => router.push('/login')}>Login</button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </div>
