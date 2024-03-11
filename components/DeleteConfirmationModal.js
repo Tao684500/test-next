2940a5e1c5c32df43f992bb9d55d4cd6bc0a3bf0
nuttawut <nuttawut@orisma.com>
2024-03-11 20:19:14 +0700

    @delete

--- components/DeleteConfirmationModal.js
+++ components/DeleteConfirmationModal.js
@@ -1,13 +1,14 @@
+// DeleteConfirmationModal.js
 import React from "react";
 import styles from "../styles/confirmation.module.css";
 
-const DeleteConfirmationModal = ({ handleConfirmRemove, toggleDeleteConfirmation }) => {
+const DeleteConfirmationModal = ({ handleConfirmRemove, toggleDeleteConfirmation, closeModal }) => {
   return (
     <div className={styles.modal}>
       <div className={styles.modal_content}>
         <p>คุณแน่ใจหรือไม่ว่าต้องการลบสินค้านี้ออกจากตะกร้า?</p>
-        <button onClick={handleConfirmRemove}>ใช่, ลบ</button>
-        <button onClick={toggleDeleteConfirmation}>ยกเลิก</button>
+        <button className={styles.checkoutButton} onClick={handleConfirmRemove}>ใช่, ลบ</button>
+        <button className={styles.closeButton} onClick={toggleDeleteConfirmation}>ยกเลิก</button>
       </div>
     </div>
   );
